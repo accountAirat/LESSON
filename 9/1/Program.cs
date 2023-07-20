2dff5f8b9d98c4757a6b2edd6d0afe824b5c16c7
Airat <mingaleev.1993@mail.ru>
2023-07-20 12:26:56 +0300

    Save commit

---
+++ 9/1/Program.cs
@@ -0,0 +1,13 @@
+﻿string[] alphavid = {"а", "и", "с", "в"};
+int size = int.Parse(Console.ReadLine()!);
+string[] combinations = new string[size];
+for (int i = 0; i < size; i++)
+{
+    for (int i = 0; i < alphavid.Length; i++)
+    {
+        for (int i = 0; i < alphavid.Length; i++)
+        {
+            
+        }
+    }
+}
